8bcb1f8b452c311f55a7be9b7e67a941b31f216c
Egor Gutsalov <egor.gutsalov@gmail.com>
2025-02-13 22:36:54 +0300

    modifided back

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 # app.py
 from flask import Flask, request, jsonify
+from model.py import Model
 
 app = Flask(__name__)
+model = Model()
 
 @app.route('/api/analyze', methods=['POST'])
 def analyze_text():
@@ -15,7 +17,7 @@ def analyze_text():
     })
 
 def analyze_sentiment(text):
-    return "neutral"
+    return model(text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

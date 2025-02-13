8f6040d540abf9910b59d2a376a76eeceec7dd40
Egor Gutsalov <egor.gutsalov@gmail.com>
2025-02-13 22:08:11 +0300

    added model class tamplate

---
+++ backend/model.py
@@ -0,0 +1,30 @@
+import random
+import spacy
+import time
+import html2text
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchtext
+from torchtext import data
+from torchtext import datasets
+import csv
+import re
+
+#TODO
+
+class Model():
+    model = "AI model"
+    tokenizer = "tokenizer"
+    traits = dict() #traits gonna be heare as dictionary
+#TODO
+#Iface
+    def __init__():
+        pass
+    
+    def get_anwser(sentance: str) -> str:
+        pass
+
+    def retrain(lines: list):
+        pass
+    
